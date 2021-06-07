7a52a965e10554b365011aa80977c3879db701e6
Joel Ring <joel.ring@gmail.com>
2021-06-07 13:00:20 +0200

    fixes rotation speed on the diagonals

--- scripts/John.gd
+++ scripts/John.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 var velocity = Vector3(0,0,0)
+var rotationVector = Vector3(0,0,0)
 const SPEED = 6
 const ROT_SPEED = 7
 const DAMPENING = 0.2
@@ -9,14 +10,17 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	rotationVector.x = 0
+	rotationVector.y = 0
+	rotationVector.z = 0
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
-		$MeshInstance.rotate_z(deg2rad(-ROT_SPEED))
+		rotationVector.z = deg2rad(-ROT_SPEED)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
-		$MeshInstance.rotate_z(deg2rad(ROT_SPEED))
+		rotationVector.z = deg2rad(ROT_SPEED)
 	else:
 		velocity.x = lerp(velocity.x, 0, DAMPENING)
 
@@ -24,15 +28,18 @@ func _physics_process(delta):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -SPEED
-		$MeshInstance.rotate_x(deg2rad(-ROT_SPEED))
+		rotationVector.x = deg2rad(-ROT_SPEED)
 	elif Input.is_action_pressed("ui_down"):
 		velocity.z = SPEED
-		$MeshInstance.rotate_x(deg2rad(ROT_SPEED))
+		rotationVector.x = deg2rad(ROT_SPEED)
 	else:
 		velocity.z = lerp(velocity.z, 0, DAMPENING)
 
 	if ((Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down")) and (Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"))):
 		velocity = velocity.normalized()
 		velocity *= SPEED
-	print (velocity)
+		rotationVector = rotationVector.normalized()
+		rotationVector *= deg2rad(ROT_SPEED)
+	$MeshInstance.rotate_x(rotationVector.x)
+	$MeshInstance.rotate_z(rotationVector.z)
 	move_and_slide(velocity)
