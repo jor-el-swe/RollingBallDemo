2ce547d67f62397d35e03cd6a81c395abc4ff3fa
Joel Ring <joel.ring@gmail.com>
2021-06-07 12:34:19 +0200

    normalizes diagonal speeds

--- scripts/John.gd
+++ scripts/John.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 var velocity = Vector3(0,0,0)
 const SPEED = 6
 const ROT_SPEED = 7
+const DAMPENING = 0.2
 
 func _ready():
 	pass
@@ -17,7 +18,7 @@ func _physics_process(delta):
 		velocity.x = -SPEED
 		$MeshInstance.rotate_z(deg2rad(ROT_SPEED))
 	else:
-		velocity.x = lerp(velocity.x, 0, 0.1)
+		velocity.x = lerp(velocity.x, 0, DAMPENING)
 
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
@@ -28,7 +29,10 @@ func _physics_process(delta):
 		velocity.z = SPEED
 		$MeshInstance.rotate_x(deg2rad(ROT_SPEED))
 	else:
-		velocity.z = lerp(velocity.z, 0, 2*delta)
+		velocity.z = lerp(velocity.z, 0, DAMPENING)
 
-
+	if ((Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down")) and (Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"))):
+		velocity = velocity.normalized()
+		velocity *= SPEED
+	print (velocity)
 	move_and_slide(velocity)
